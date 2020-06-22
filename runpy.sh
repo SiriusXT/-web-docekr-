381a45739e9b85f2e4e30b35cd6aaca458579eaf
1160925501@qq.com <1160925501@qq.com>
2020-06-22 15:48:30 +0800

    add

--- runpy.sh
+++ runpy.sh
@@ -1,4 +1,4 @@
 echo start
-python3 login/login.py > log-login.txt -D
-python3 admin/ad.py  > log-ad.txt -D
-python3 user/user.py  > log-user.txt -D
+python3 login/login.py > log-login.txt &
+python3 admin/ad.py  > log-ad.txt &
+python3 user/user.py  > log-user.txt &
