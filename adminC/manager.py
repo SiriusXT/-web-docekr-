0d5c21ca41e47583cc9fcf825b8c91c723b6cb2d
1160925501@qq.com <1160925501@qq.com>
2020-06-27 14:49:25 +0800

    1

--- adminC/manager.py
+++ adminC/manager.py
@@ -248,6 +248,7 @@ class UserHandler(tornado.web.RequestHandler):
             sql = """INSERT INTO containers(id,
                      username , ip)
                      VALUES ("""+ss[0][0:12]+""", """+username+""", "1")"""
+            print(sql)
             cursor.execute(sql)
             db.commit()
             cursor.close()
